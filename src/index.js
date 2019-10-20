89acd733c9886a64cd16230b5097fd168e3239eb
Franco Meriles <fran.meriles@gmail.com>
2019-10-20 17:09:44 -0300

    clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //require("./db/db");
 const app = require("./app");
-//IIFE START APP
 
-(async function main() {
+//IIFE START APP
+(async () => {
   await app.listen(app.get("port"));
   console.log(`Server on Port ${app.get("port")}`);
 })();
